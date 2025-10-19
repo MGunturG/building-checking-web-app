b94b5b55fff9bd74e191fbe48911291b7546f824
Muhamad Guntur <39583921+MGunturG@users.noreply.github.com>
2025-10-19 14:28:42 +0700

    check if the user already loggen in or not

--- app/index.php
+++ app/index.php
@@ -1,3 +1,11 @@
-<?php 
-header("location:login.php");
+<?php
+// as usuall, start session
+session_start();
+
+// check if the user already logged in or not
+if($_SESSION['login_status'] == "logged_in") {
+	header("location:dashboard/index.php");
+} else {
+	header("location:login.php?login_status=not_logged_in");
+}
 ?>
